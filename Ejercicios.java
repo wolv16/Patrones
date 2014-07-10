2e6e4f2739c46ac2e4eff4fc78700c8a87350ab0
David Ramirez <dabid999@gmail.com>
2014-07-09 20:41:57 -0500

    Update Ejercicios.java

--- Ejercicios.java
+++ Ejercicios.java
@@ -1,94 +1,108 @@
-/**
- * @(#)Ejercicio.java
- *
- *
- * @author David Ramirez
- * @version 1.00 2014/7/8
- */
-import javax.swing.JOptionPane;
-
-public class Ejercicio {
-	
-	private int puntosA;
+import javax.swing.*;
+import java.awt.event.*;
+public class Ejercicio extends JFrame implements ActionListener {
+    private int puntosA;
 	private int puntosB;
 	private String nombreA;
 	private String nombreB;
 	private String frases[] = {"love","all","deuce","fifteen","thirty","fourty","adventages player 1"
-								,"adventages player 2","player 1 wins","player 2 wins"};
-	
+								,"adventages player 2","wins"};
+    JButton boton1;
+    JButton boton2;
+    
     public Ejercicio(String nombreA, String nombreB) {
-    	this.nombreA = nombreA;
+		this.nombreA = nombreA;
     	this.nombreB = nombreB;
     	puntosA = 0;
     	puntosB = 0;
-    }
-    public void juega(){
-    	String mensaje = frases[0]+" - "+frases[1];
-    	while (puntosA <= 60 && puntosB <= 60){
-    		System.out.print(puntosA+" -- "+ puntosB+"    ");
-    		System.out.println(mensaje);
-    		int jugador = Integer.parseInt(JOptionPane.showInputDialog("¿Qué jugador anotó?","Ingresa 1 o 2"));
-    		if(jugador == 1){
-    			puntoA();
-    		}
-    		if(jugador == 2){
-    			puntoB();
-    		}
-    		if (puntosA == puntosB){
-    			if (puntosA <= 30 || puntosB < 30){
-	    			if(puntosA == 15){
-	    				mensaje = frases[3]+" - "+frases[1];
-	    			}
-		    		if(puntosA == 30){
-		    			mensaje = frases[4]+" - "+frases[1];
-		    		}	
-    			}else{
-    				mensaje = frases[2];
-    			}
-    		}
-    		else{
-    			if(puntosA == 0){
-    				mensaje = frases[0]+" - ";
-    			}
-    			if(puntosA == 15){
-    				mensaje = frases[3]+" - ";
-    			}
-	    		if(puntosA == 30){
-	    			mensaje = frases[4]+" - ";
-	    		}
-	    		if(puntosA == 40){
-	    			mensaje = frases[5]+" - ";
-	    		}
-	    		if ((puntosB > 40) && (puntosB > puntosA)){
-	    			mensaje = frases[7];
-	    		}
-	    		
-	    		if(puntosB == 0){
-    				mensaje += frases[0];
-    			}
-	    		if(puntosB == 15){
-    				mensaje += frases[3];
-    			}
-	    		if(puntosB == 30){
-	    			mensaje += frases[4];
-	    		}
-	    		if(puntosB == 40){
-	    			mensaje += frases[5];
-	    		}
-	    		if ((puntosA > 40) && (puntosA > puntosB)){
-	    			mensaje = frases[6];
-	    		}
-    		}
-    	}
-    	if (puntosA > puntosB){
-    		mensaje = frases[8];
-    	}
-    	else {
-    		mensaje = frases[9];
-    	}
-    	System.out.println(mensaje);
+    	
+        setLayout(null);
+        boton1=new JButton(nombreA);
+        boton1.setBounds(100,100,100,30);
+        add(boton1);
+        boton1.addActionListener(this);
+        
+        boton2=new JButton(nombreB);
+        boton2.setBounds(300,250,100,30);
+        add(boton2);
+        boton2.addActionListener(this);
+        this.setDefaultCloseOperation(Ejercicio.DISPOSE_ON_CLOSE);
+        System.out.println(frases[0]+" - "+frases[1]);
     }
     
+    public void actionPerformed(ActionEvent e) {
+    	if (puntosA < 60 && puntosB < 60){
+	        if (e.getSource()==boton2) {
+	            juega(2);
+	        }
+	        if (e.getSource()==boton1) {
+	            juega(1);
+	        }
+    	}else{
+    	   	if (puntosA > puntosB){
+	    		System.out.println(nombreA+" "+frases[8]);
+	    	}
+	    	else {
+	    		System.out.println(nombreB+" "+frases[8]);
+	    	}
+    	}
+    }
+    public void juega(int n){
+    	String mensaje ="";    	    		
+		if(n == 1){
+			puntoA();
+		}
+		if(n == 2){
+			puntoB();
+		}
+		if (puntosA == puntosB){
+			if (puntosA <= 30 || puntosB < 30){
+    			if(puntosA == 15){
+    				mensaje = frases[3]+" - "+frases[1];
+    			}
+	    		if(puntosA == 30){
+	    			mensaje = frases[4]+" - "+frases[1];
+	    		}	
+			}else{
+				mensaje = frases[2];
+			}
+		}
+		else{
+			if(puntosA == 0){
+				mensaje = frases[0]+" - ";
+			}
+			if(puntosA == 15){
+				mensaje = frases[3]+" - ";
+			}
+    		if(puntosA == 30){
+    			mensaje = frases[4]+" - ";
+    		}
+    		if(puntosA == 40){
+    			mensaje = frases[5]+" - ";
+    		}
+    		if ((puntosB > 40) && (puntosB > puntosA)){
+    			mensaje = frases[7];
+    		}
+    		
+    		if(puntosB == 0){
+				mensaje += frases[0];
+			}
+    		if(puntosB == 15){
+				mensaje += frases[3];
+			}
+    		if(puntosB == 30){
+    			mensaje += frases[4];
+    		}
+    		if(puntosB == 40){
+    			mensaje += frases[5];
+    		}
+    		if ((puntosA > 40) && (puntosA > puntosB)){
+    			mensaje = frases[6];
+    		}
+		}
+		System.out.print(puntosA+" -- "+ puntosB+"    ");
+    	System.out.println(mensaje);
+    }
     public void puntoA(){
     	if(puntosA <= 15){
     		puntosA += 15;
