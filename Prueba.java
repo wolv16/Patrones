11f01052360135c7ea875ca57341a68f9aa324cd
David Ramirez <dabid999@gmail.com>
2014-07-09 20:39:45 -0500

    Create Prueba.java

---
+++ Prueba.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)Prueba.java
+ *
+ *
+ * @author 
+ * @version 1.00 2014/7/8
+ */
+
+
+public class Prueba {
+
+    public static void main (String David []) {
+    	Ejercicio ejercicio = new Ejercicio ("David", "Eliel");
+    	ejercicio.juega();
+    	System.out.println(ejercicio.score());
+    }
+}
